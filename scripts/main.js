62af7e40b92606fba8af54549279a921295b5081
Brady Logan <brady.drew.logan@gmail.com>
2020-08-27 15:48:22 -0500

    created doctor factory function which accepts three parameters

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//Lightning Exercises
+
+//Doctor
+
+const createDoctor = (name, specialty, practice) => {
+    return {
+        name: name,
+        specialty: specialty,
+        practice: practice
+    }
+}
+
+const doctor = createDoctor("Fauci", "Diseases", "White House");
+//console.log(doctor);
+
